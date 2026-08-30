a78f1634f4d7ec64f00b36aef9c795827fb1a2ac
minsaadun <minsaadun@gmail.com>
2026-08-31 00:47:52 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
+  base: '/web-php-database/',
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
